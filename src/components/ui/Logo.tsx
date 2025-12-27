0fd9d7810750635fc1a7f9834047d4780e33e9e0
cscottburch1 <scott.burchcontracting@gmail.com>
2025-12-26 20:33:42 -0500

    Add cache-busting parameter to force logo refresh

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -18,7 +18,7 @@ export function Logo({ variant = 'header', className = '', href = '/' }: LogoPro
 
   const logoImage = (
     <Image
-      src="/logo.png"
+      src="/logo.png?v=2"
       alt="Burch Contracting Logo"
       width={size.width}
       height={size.height}
